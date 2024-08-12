b437e11e93859383865192e6c98a022deed03a38
ioannis-xenakis <Xenakis.i.contact@gmail.com>
2024-08-12 18:57:51 +0300

    Create columns for tech stacks database table
    Create columns(for "up" migrations) for tech stacks database table.

--- database/migrations/2024_08_08_111451_create_tech_stacks_table.php
+++ database/migrations/2024_08_08_111451_create_tech_stacks_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('tech_stacks', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
+            $table->string("icon");
             $table->timestamps();
         });
     }

85644f94312a9016c2257ee5556b7b184cc8cff7
ioannis-xenakis <Xenakis.i.contact@gmail.com>
2024-08-22 22:06:29 +0300

    Add column names in education php model
    Add column names in Education.php model.

--- app/Models/Education.php
+++ app/Models/Education.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Education extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "education_name",
+        "education_location",
+        "education_year",
+        "education_description"
+    ];
 }

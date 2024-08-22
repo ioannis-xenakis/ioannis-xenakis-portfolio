a5e101aaa979c3f95adec162bcb80b9f5f4ea387
ioannis-xenakis <Xenakis.i.contact@gmail.com>
2024-08-22 21:47:29 +0300

    Add column names in settings php model
    Add column names in Settings.php model.

--- app/Models/Settings.php
+++ app/Models/Settings.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Settings extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        "first_english_name",
+        "first_greek_name",
+        "last_name",
+        "job_title",
+        "location",
+        "phone",
+        "email",
+        "languages",
+        "birth_date",
+        "github_url",
+        "stack_overflow_url",
+        "linkedin_url",
+        "google_play_url"
+    ];
 }

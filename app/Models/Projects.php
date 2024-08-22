f7805c50c43ffabe059afa311b9419ad568d0c5c
ioannis-xenakis <Xenakis.i.contact@gmail.com>
2024-08-22 21:53:39 +0300

    Add column names in projects php model
    Add column names in Projects.php model.

--- app/Models/Projects.php
+++ app/Models/Projects.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Projects extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "project_name",
+        "project_description",
+        "project_image",
+        "keywords",
+        "github_url",
+        "google_play_url"
+    ];
 }

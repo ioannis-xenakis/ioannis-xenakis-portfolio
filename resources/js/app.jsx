034f3b36ce6db52adb0cc958c2b82b68c31bfedd
ioannis-xenakis <Xenakis.i.contact@gmail.com>
2024-09-19 19:25:22 +0300

    Set inertia app id
    Set inertia app name(id).

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -4,6 +4,7 @@ import { createInertiaApp } from '@inertiajs/react'
 import { createRoot } from 'react-dom/client'
 
 createInertiaApp({
+  id: 'ioannis-xenakis-portfolio',
   resolve: name => {
     const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true })
     return pages[`./Pages/${name}.jsx`]

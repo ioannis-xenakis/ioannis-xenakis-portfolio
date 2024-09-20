7e6b8077493f1b2e1190875a08c34f54e0f7365f
ioannis-xenakis <Xenakis.i.contact@gmail.com>
2024-09-20 18:44:28 +0300

    Add react in vite config
    Add react plugin in vite config.

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
@@ -7,5 +8,6 @@ export default defineConfig({
             input: 'resources/js/app.jsx',
             refresh: true,
         }),
+        react(),
     ],
 });

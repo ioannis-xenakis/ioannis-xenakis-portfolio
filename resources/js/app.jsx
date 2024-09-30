6c3d2e4da9ba60888a8b0b83483a6ae5bc543f22
ioannis-xenakis <Xenakis.i.contact@gmail.com>
2024-09-30 04:03:08 +0300

    Remove custom root element in app.jsx

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -5,7 +5,6 @@ import { createInertiaApp } from '@inertiajs/react'
 import { createRoot } from 'react-dom/client'
 
 createInertiaApp({
-  id: 'ioannis-xenakis-portfolio',
   resolve: name => {
     const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true })
     return pages[`./Pages/${name}.jsx`]

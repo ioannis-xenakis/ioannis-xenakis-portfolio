51b3df50913dbeedf4c3b41d4bff7a594a88728b
ioannis-xenakis <Xenakis.i.contact@gmail.com>
2024-10-09 10:23:59 +0300

    Center main content

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
-        <>
+        <div className="main-content">
             <h1 className="home-page-text">Hey user</h1>
-        </>
+        </div>
     );
 }

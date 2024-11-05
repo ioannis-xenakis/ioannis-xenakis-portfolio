732560dda1ae4f576d8c3d70b1bf4910c38800f8
ioannis-xenakis <Xenakis.i.contact@gmail.com>
2024-11-05 20:40:09 +0200

    Re center main home page text
    Re-center home page's main text.

--- resources/js/Layouts/Layout.jsx
+++ resources/js/Layouts/Layout.jsx
@@ -11,7 +11,7 @@ export default function Layout({children}) {
                 </nav>
             </header>
 
-            <main>
+            <main className="main-content">
                 {children}
             </main>
         </>

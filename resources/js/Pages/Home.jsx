d553a59ccc79ebd0324265f4c398b8c331d4b0d5
ioannis-xenakis <Xenakis.i.contact@gmail.com>
2024-12-18 02:24:55 +0200

    Add text when photo is missing
    Replace john xenakis photo with text, when john xenakis photo is missing.

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -3,7 +3,7 @@ import TerminalIcon from '../Assets/Terminal-Icon'
 export default function Home({photos}) {
     return (
         <div className="main-content">
-            <img src={photos.photo_url}></img>
+            <img src={photos.photo_url} alt="John Xenakis photo"></img>
             <div className="im-john-box">
                 <div className="terminal-icon"> <TerminalIcon/> </div>
                 <h1 className="home-page-text"> I'm <br/><span className="blue-text">John</span> </h1>

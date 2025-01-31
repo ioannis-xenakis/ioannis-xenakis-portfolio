f08ed3f84637db797f64ff12caeaed3551b6d158
ioannis-xenakis <Xenakis.i.contact@gmail.com>
2025-01-31 17:00:46 +0200

    Feat: Create empty about screen
    Create empty "About" screen/page.
    
    Resolves: CU-86c1wbzn9[complete]

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,11 @@
 
 use App\Http\Controllers\PhotosController;
 use App\Http\Controllers\HomePageController;
+use App\Http\Controllers\AboutPageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomePageController::class, 'index']);
+
+Route::get('/about', [AboutPageController::class, 'index']);
 
 Route::resource('photos', PhotosController::class)->except('index');

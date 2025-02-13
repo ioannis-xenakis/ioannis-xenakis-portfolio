3944ea025e74131d44cd3fae336c8f2e3a411876
ioannis-xenakis <Xenakis.i.contact@gmail.com>
2025-02-13 13:45:40 +0200

    Feat: Add personal info items
    Create personal info items in about page.
    
    Resolves: CU-86c1wb69p[complete]

--- resources/js/Pages/About.jsx
+++ resources/js/Pages/About.jsx
@@ -1,6 +1,17 @@
 import InfoIcon from "../Assets/Info-Icon";
 
 export default function About({ setting, photo }) {
+    function getAge(dateString) {
+        var today = new Date();
+        var birthDate = new Date(dateString);
+        var age = today.getUTCFullYear() - birthDate.getFullYear();
+        var m = today.getMonth() - birthDate.getMonth();
+        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+            age--;
+        }
+        return age;
+    }
+
     return (
         <div className="about-page-content">
             <div className="about-page-title">
@@ -20,6 +31,32 @@ export default function About({ setting, photo }) {
                         src={photo.photo_url}
                         alt="John Xenakis photo"
                     />
+                </div>
+            </div>
+            <div className="about-page-personal-info">
+                <div className="about-page-item">
+                    <span className="main-text">
+                        Name: {setting.first_greek_name}/
+                        {setting.first_english_name} {setting.last_name}
+                    </span>
+                </div>
+                <div className="about-page-item">
+                    <span className="main-text">
+                        Location: {setting.location}
+                    </span>
+                </div>
+                <div className="about-page-item">
+                    <span className="main-text">
+                        Age: {getAge(setting.birth_date)} y.o.
+                    </span>
+                </div>
+                <div className="about-page-item">
+                    <span className="main-text">
+                        Languages: {setting.languages}
+                    </span>
+                </div>
+                <div className="about-page-item">
+                    <span className="main-text">Pronouns: he/him/his</span>
                 </div>
             </div>
         </div>

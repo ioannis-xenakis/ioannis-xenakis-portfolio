c64b50eb6daa74cfb53269824eb3159a217e4ab0
ioannis-xenakis <Xenakis.i.contact@gmail.com>
2025-02-27 22:29:05 +0200

    Add projects page title text

--- resources/js/Pages/Projects.jsx
+++ resources/js/Pages/Projects.jsx
@@ -1,8 +1,11 @@
-export default function Projects() {
+export default function Projects({ setting }) {
     return (
         <div className="projects-page-content">
             <div className="projects-page-title">
-                <h2 className="projects-page-title-text">Projects</h2>
+                <h2 className="projects-page-title-text">
+                    {setting.first_english_name.LowerCase()}.
+                    <span className="blue-text">projects</span>
+                </h2>
             </div>
         </div>
     );

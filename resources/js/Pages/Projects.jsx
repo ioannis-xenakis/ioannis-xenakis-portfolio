6547ab7fadeaec64d2eb2081892aa1c5bd9c8993
ioannis-xenakis <Xenakis.i.contact@gmail.com>
2025-03-12 11:18:47 +0200

    Feat: Create projects page title icon
    Resolves: CU-86c29t5zx[complete]

--- resources/js/Pages/Projects.jsx
+++ resources/js/Pages/Projects.jsx
@@ -1,7 +1,10 @@
+import TerminalIcon2 from "../Assets/Terminal-Icon-2";
+
 export default function Projects({ setting }) {
     return (
         <div className="projects-page-content">
             <div className="projects-page-title">
+                <TerminalIcon2 />
                 <h2 className="projects-page-title-text">
                     {setting.first_english_name.toLowerCase()}.
                     <span className="blue-text">projects</span>

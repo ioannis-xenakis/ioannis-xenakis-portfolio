6e1eb10462160d9532f780142cc7975a6848bab9
ioannis-xenakis <Xenakis.i.contact@gmail.com>
2025-03-14 02:14:24 +0200

    Rename github and google play icons
    Rename github and google play icons in projects seeder, to github_icon_2 and google_play_icon_2 as there is 2 github and google play icons.

--- database/seeders/ProjectsSeeder.php
+++ database/seeders/ProjectsSeeder.php
@@ -17,8 +17,8 @@ class ProjectsSeeder extends Seeder
             "project_description" => "“My Notie” is an android notes app made for being simple and pleasant to work with your notes.",
             "project_image_url" => "storage/images/my_notie.png",
             "keywords" => json_encode(["Kotlin", "XML", "Room DB", "Native Android App"]),
-            "github_icon_url" => "storage/images/github_icon.svg",
-            "google_play_icon_url" => "storage/images/google_play_icon.svg",
+            "github_icon_url" => "storage/images/github_icon_2.svg",
+            "google_play_icon_url" => "storage/images/google_play_icon_2.svg",
             "github_url" => "https://github.com/ioannis-xenakis/my-notie",
             "google_play_url" => "https://play.google.com/store/apps/details?id=com.xenakis.my_notie"
         ]);
@@ -28,8 +28,8 @@ class ProjectsSeeder extends Seeder
             "project_description" => "An android app called “Jokester” that is made for people to see some nice jokes and laugh. ",
             "project_image_url" => "storage/images/jokester.png",
             "keywords" => json_encode(["Kotlin", "Jetpack Compose", "Retrofit", "Native Android App"]),
-            "github_icon_url" => "storage/images/github_icon.svg",
-            "google_play_icon_url" => "storage/images/google_play_icon.svg",
+            "github_icon_url" => "storage/images/github_icon_2.svg",
+            "google_play_icon_url" => "storage/images/google_play_icon_2.svg",
             "github_url" => "https://github.com/ioannis-xenakis/jokester",
             "google_play_url" => "https://play.google.com/store/apps/details?id=com.xenakis.jokester"
         ]);
@@ -39,7 +39,7 @@ class ProjectsSeeder extends Seeder
             "project_description" => "It’s the same site that you are visiting right now! It is about my portfolio site (Ioannis Xenakis portfolio).",
             "project_image_url" => "storage/images/portfolio.png",
             "keywords" => json_encode(["PHP", "Laravel", "XAMPP MySQL", "Inertia React JS", "Tailwind CSS"]),
-            "github_icon_url" => "storage/images/github_icon.svg",
+            "github_icon_url" => "storage/images/github_icon_2.svg",
             "google_play_icon_url" => "",
             "github_url" => "https://github.com/ioannis-xenakis/ioannis-xenakis-portfolio",
             "google_play_url" => ""

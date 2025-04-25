db1f167e83ed74872e7f2ed0e06cc4ef33d16855
ioannis-xenakis <Xenakis.i.contact@gmail.com>
2025-04-25 15:13:30 +0300

    Remove resolve alias in vite config js

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,4 @@ export default defineConfig({
         }),
         react(),
     ],
-    resolve: {
-        alias: {
-            '@': resolve(__dirname, '/resources/js'),
-        },
-    },
 });
